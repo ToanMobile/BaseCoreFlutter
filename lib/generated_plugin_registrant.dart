7f1f0f1d3fa12d23e16ba9998bd776984699715a
ToanMobile <hvtoan.dev@gmail.com>
2022-07-04 10:41:26 +0700

    update sdk.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,11 +4,13 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
 import 'package:camera_web/camera_web.dart';
 import 'package:connectivity_for_web/connectivity_for_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
